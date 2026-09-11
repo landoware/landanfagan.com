2c5198de474d4f6f2a74409bc7ad2e7da6e5bfd4
Landan Fagan <landan@anolar.net>
2026-09-11 13:44:14 -0600

    + About output

--- src/components/outputs/AboutOutput.vue
+++ src/components/outputs/AboutOutput.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="mb-2">
-    <p class="text-fg-dark">// TODO: write about section</p>
+    <p class="text-fg-dark">
+      Software engineer working in Laravel, Go, and Vue. I focus on the unglamorous but important
+      parts of systems that keep things running smoothly.
+    </p>
   </div>
 </template>
